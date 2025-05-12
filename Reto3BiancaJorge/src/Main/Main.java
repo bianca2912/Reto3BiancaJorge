0da05b4783cdafeedca91fe2884ca5e81ac3677d
jorge <jorgeruedaplo2004@gmail.com>
2025-05-12 13:54:01 +0200

    Creando menu

--- Reto3BiancaJorge/src/Main/Main.java
+++ Reto3BiancaJorge/src/Main/Main.java
@@ -1,10 +1,20 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int numero=0;
+		do {
+			menu();
+			numero=FuncionesPK.Funciones.dimeEntero("Elige un menu", sc);
+		} while (numero>0);
 
+	}
+	public static void menu() {
+		System.out.println("1. Mantenimientos \n2. Catalogo de Productos \n3. Pedidos \n4. Informes \n5. Salir");
 	}
 
 }
