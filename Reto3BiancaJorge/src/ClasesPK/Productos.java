a90f311e945b4f09b08aeaa574c7a3b919c236b9
jorge <jorgeruedaplo2004@gmail.com>
2025-05-12 13:38:39 +0200

    Actualizando Clase Productos

--- Reto3BiancaJorge/src/ClasesPK/Productos.java
+++ Reto3BiancaJorge/src/ClasesPK/Productos.java
@@ -1,5 +1,73 @@
 package ClasesPK;
 
 public class Productos {
-
+	private int idProducto;
+	private int idCategoria;
+	private String nombre;
+	private double precio;
+	private String descripcion;
+	private String color;
+	private String talla;
+	private int stock;
+	public Productos(int idProducto, int idCategoria, String nombre, double precio, String descripcion, String color,
+			String talla, int stock) {
+		super();
+		this.idProducto = idProducto;
+		this.idCategoria = idCategoria;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripcion = descripcion;
+		this.color = color;
+		this.talla = talla;
+		this.stock = stock;
+	}
+	public int getIdProducto() {
+		return idProducto;
+	}
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getTalla() {
+		return talla;
+	}
+	public void setTalla(String talla) {
+		this.talla = talla;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
 }
