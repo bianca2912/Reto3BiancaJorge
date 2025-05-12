05d3a95f0857bc75ed159331da1a4c85cf6383b6
jorge <jorgeruedaplo2004@gmail.com>
2025-05-12 13:06:12 +0200

    Actualizando Clase Categoria

--- Reto3BiancaJorge/src/ClasesPK/Categorias.java
+++ Reto3BiancaJorge/src/ClasesPK/Categorias.java
@@ -1,5 +1,24 @@
 package ClasesPK;
 
 public class Categorias {
-
+	private int idCategoria;
+	private String nombre;
+	public Categorias(int idCategoria, String nombre) {
+		super();
+		this.idCategoria = idCategoria;
+		this.nombre = nombre;
+	}
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
