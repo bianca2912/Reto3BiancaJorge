5dd3726f66b28499f1244c23a017f36d30d15353
bianca2912 <DAM1@aula11-pc26>
2025-05-12 13:26:56 +0200

    Actualizado CategoriasDAO

--- Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
+++ Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
@@ -1,7 +1,45 @@
 package ClasesDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import Conexion.Conexion;
+
 public class CategoriasDAO {
 
-	
-	
+    public static void insertarCategoria() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce el nombre de la nueva categoría: ");
+        String nombre = sc.nextLine();
+        
+        if (nombre.isEmpty()) {
+            System.out.println("El nombre no puede estar vacío.");
+            return;
+        }
+
+        String sql = "INSERT INTO categoria (nombre) VALUES (?)";
+
+        try (Connection conn = Conexion.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+               stmt.setString(1, nombre);
+               int filas = stmt.executeUpdate();
+
+               if (filas > 0) {
+                   System.out.println("Categoría insertada correctamente.");
+               } else {
+                   System.out.println("No se pudo insertar la categoría.");
+               }
+
+           } catch (SQLException e) {
+               System.out.println("Error al insertar categoría.");
+               e.printStackTrace();
+           }
+    }
+  
+
+
 }
+
