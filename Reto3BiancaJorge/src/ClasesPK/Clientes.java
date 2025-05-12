c552b0b65eaa7b82d143aa66ba666f282a2a7357
jorge <jorgeruedaplo2004@gmail.com>
2025-05-12 13:22:03 +0200

    Actualizando la Clase Cliente

--- Reto3BiancaJorge/src/ClasesPK/Clientes.java
+++ Reto3BiancaJorge/src/ClasesPK/Clientes.java
@@ -1,5 +1,40 @@
 package ClasesPK;
 
 public class Clientes {
-	private 
+	private int idCliente;
+	private String nombre;
+	private String direccion;
+	private int codigo;
+	public Clientes(int idCliente, String nombre, String direccion, int codigo) {
+		super();
+		this.idCliente = idCliente;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.codigo = codigo;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
 }
