717a8e956ae32c4746a72c639c54bf0dd493642f
jorge <jorgeruedaplo2004@gmail.com>
2025-05-12 13:34:28 +0200

    Actualizando Clase Pedidos

--- Reto3BiancaJorge/src/ClasesPK/Pedidos.java
+++ Reto3BiancaJorge/src/ClasesPK/Pedidos.java
@@ -1,5 +1,51 @@
 package ClasesPK;
 
-public class Pedidos {
+import java.util.Date;
 
+public class Pedidos {
+	private int idPedido;
+	private int idCliente;
+	private double precioTotal;
+	private String direccionEnvio;
+	private Date fecha;
+	public Pedidos(int idPedido, int idCliente, double precioTotal, String direccionEnvio, Date fecha) {
+		super();
+		this.idPedido = idPedido;
+		this.idCliente = idCliente;
+		this.precioTotal = precioTotal;
+		this.direccionEnvio = direccionEnvio;
+		this.fecha = fecha;
+	}
+	public int getIdPedido() {
+		return idPedido;
+	}
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+	public String getDireccionEnvio() {
+		return direccionEnvio;
+	}
+	public void setDireccionEnvio(String direccionEnvio) {
+		this.direccionEnvio = direccionEnvio;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
 }
