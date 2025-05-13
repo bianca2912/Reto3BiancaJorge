95a5797a0e059a93b917ee667d18d777abf78862
jorge <jorgeruedaplo2004@gmail.com>
2025-05-13 08:39:29 +0200

    Actualizando el Main

--- Reto3BiancaJorge/src/Main/Main.java
+++ Reto3BiancaJorge/src/Main/Main.java
@@ -8,13 +8,45 @@ public class Main {
 		Scanner sc=new Scanner(System.in);
 		int numero=0;
 		do {
-			menu();
-			numero=FuncionesPK.Funciones.dimeEntero("Elige un menu", sc);
-		} while (numero>0);
+			try {
+				menu();
+				numero=FuncionesPK.Funciones.dimeEntero("Elige un menu", sc);
+				switch (numero) {
+				case 1:
+					System.out.println("1");
+					break;
+				case 2:
+					System.out.println("2");		
+					break;
+				case 3:
+					System.out.println("3");
+					break;
+				case 4:
+					System.out.println("4");
+					break;
+				}
+				if (numero==5) {
+					System.out.println("Has salido del menu");
+					break;
+				}
+				errorMenu(numero);
+			} catch (Exception e) {
+				e.printStackTrace();
+				
+			}
+			
+			
+		} while (numero>0 && numero<6);	
+		
 
 	}
 	public static void menu() {
 		System.out.println("1. Mantenimientos \n2. Catalogo de Productos \n3. Pedidos \n4. Informes \n5. Salir");
+	}
+	public static void errorMenu(int numero) {
+		if (!(numero>0 && numero<6)) {
+			System.out.println("El numero no coincide con el menu");
+		}
 	}
 
 }
