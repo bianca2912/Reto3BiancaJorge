8330269398d558b7cb4698a42173c15787a84e37
bianca2912 <DAM1@aula11-pc26>
2025-05-13 08:39:18 +0200

    He actualizado la clase CategoriasDAO haciendo que se pida en la clase main

--- Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
+++ Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
@@ -9,36 +9,30 @@ import Conexion.Conexion;
 
 public class CategoriasDAO {
 
-    public static void insertarCategoria() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce el nombre de la nueva categoria: ");
-        String nombre = sc.nextLine();
-        
-        if (nombre.isEmpty()) {
-            System.out.println("El nombre no puede estar vacio.");
-            return;
-        }
+    	public static void insertarCategoria(String nombre) {
+            if (nombre.isEmpty()) {
+                System.out.println("El nombre no puede estar vacío.");
+                return;
+            }
 
-        String sql = "INSERT INTO categoria (nombre) VALUES (?)";
+            String sql = "INSERT INTO categoria (nombre) VALUES (?)";
 
-        try (Connection conn = Conexion.conectar();
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            try (Connection conn = Conexion.conectar();
+                 PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-               stmt.setString(1, nombre);
-               int filas = stmt.executeUpdate();
+                stmt.setString(1, nombre);
+                int filas = stmt.executeUpdate();
 
-               if (filas > 0) {
-                   System.out.println("Categoria insertada correctamente.");
-               } else {
-                   System.out.println("No se pudo insertar la categoria.");
-               }
+                if (filas > 0) {
+                    System.out.println("Categoría insertada correctamente.");
+                } else {
+                    System.out.println("No se pudo insertar la categoría.");
+                }
 
-           } catch (SQLException e) {
-               System.out.println("Error al insertar categoria.");
-               e.printStackTrace();
-           }
+            } catch (SQLException e) {
+                System.out.println("Error al insertar categoría.");
+                e.printStackTrace();
+            }
     }
-   
-    
 }
 
