de546efe316f7ead952ea6a6f6854346927a737a
jorge <jorgeruedaplo2004@gmail.com>
2025-05-13 09:16:47 +0200

    Comprobando ProductosDao

--- Reto3BiancaJorge/src/Main/Main.java
+++ Reto3BiancaJorge/src/Main/Main.java
@@ -3,6 +3,7 @@ package Main;
 import java.util.Scanner;
 
 import ClasesDAO.CategoriasDAO;
+import ClasesDAO.ProductosDAO;
 
 public class Main {
 
@@ -27,7 +28,7 @@ public class Main {
 					System.out.println("4");
 					break;
 				}
-				if (numero==5) {
+				if (numero==0) {
 					System.out.println("Has salido del menu");
 					break;
 				}
@@ -38,19 +39,33 @@ public class Main {
 			}
 			
 			
-		} while (numero>0 && numero<6);	
+		} while (numero>0 && numero<5);	
 		
-		System.out.println("Nueva categoria");
+	/*	CategoriasDao
+	 * 
+	 * System.out.println("Nueva categoria");
 		String n=sc.nextLine();
 		CategoriasDAO.insertarCategoria(n);
-		
+	*/
+		System.out.println("nombre, talla, color, stock, precio, idCategoria");
+		String nomb=sc.nextLine();
+		String talla=sc.nextLine();
+		String color=sc.nextLine();
+		int stock=sc.nextInt();
+		double precio=sc.nextDouble();
+		int id=sc.nextInt();
+		ProductosDAO.insertarProducto(nomb, talla, color, stock, precio, id);
+		//nombre, talla, color, stock, precio, idCategoria
 
 	}
 	public static void menu() {
-		System.out.println("1. Mantenimientos \n2. Catalogo de Productos \n3. Pedidos \n4. Informes \n5. Salir");
+		System.out.println("1. Mantenimientos \n2. Catalogo de Productos \n3. Pedidos \n4. Informes \n0. Salir");
+	}
+	public static void subMenuMantenimiento() {
+		System.out.println("1. Mantenimientos \n2. Catalogo de Productos \n3. Pedidos \n4. Informes \n0. Salir");
 	}
 	public static void errorMenu(int numero) {
-		if (!(numero>0 && numero<6)) {
+		if (!(numero>0 && numero<5)) {
 			System.out.println("El numero no coincide con el menu");
 		}
 	}
