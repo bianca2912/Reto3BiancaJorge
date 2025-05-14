b16ec8fc564000b871a3e8eb6960791e921fee50
jorge <jorgeruedaplo2004@gmail.com>
2025-05-14 12:49:32 +0200

    Actualizado ClientesDao

--- Reto3BiancaJorge/src/ClasesDAO/ClientesDAO.java
+++ Reto3BiancaJorge/src/ClasesDAO/ClientesDAO.java
@@ -9,20 +9,20 @@ import Conexion.Conexion;
 
 public class ClientesDAO {
 
-	  public static void insertarCliente(String nombre, String direccion, String email) {
-	        if (nombre.isEmpty() || direccion.isEmpty() || email.isEmpty()) {
+	  public static void insertarCliente(String nombre, String direccion, int codigo) {
+	        if (nombre.isEmpty() || direccion.isEmpty() || codigo==0) {
 	            System.out.println("Los campos no pueden estar vacios.");
 	            return;
 	        }
 
-	        String sql = "INSERT INTO cliente (nombre, direccion, email) VALUES (?, ?, ?)";
+	        String sql = "INSERT INTO clientes (nombre, direccion, codigo) VALUES (?, ?, ?)";
 
 	        try (Connection conn = Conexion.conectar();
 	             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
 	            stmt.setString(1, nombre);
 	            stmt.setString(2, direccion);
-	            stmt.setString(3, email);
+	            stmt.setInt(3, codigo);
 
 	            int filas = stmt.executeUpdate();
 
@@ -38,7 +38,7 @@ public class ClientesDAO {
 	        }
 	    }
 
-	    public static void buscarYModificarCliente(int id, String nuevoNombre, String nuevaDireccion, String nuevoEmail) {
+	    public static void buscarYModificarCliente(int id, String nuevoNombre, String nuevaDireccion, int nuevoCodigo) {
 	        String sqlBuscar = "SELECT * FROM cliente WHERE id = ?";
 
 	        try (Connection conn = Conexion.conectar();
@@ -48,11 +48,11 @@ public class ClientesDAO {
 	            ResultSet rs = stmt.executeQuery();
 
 	            if (rs.next()) {
-	                String sqlUpdate = "UPDATE cliente SET nombre = ?, direccion = ?, email = ? WHERE id = ?";
+	                String sqlUpdate = "UPDATE cliente SET nombre = ?, direccion = ?, codigo = ? WHERE id = ?";
 	                try (PreparedStatement stmtUpdate = conn.prepareStatement(sqlUpdate)) {
 	                    stmtUpdate.setString(1, nuevoNombre);
 	                    stmtUpdate.setString(2, nuevaDireccion);
-	                    stmtUpdate.setString(3, nuevoEmail);
+	                    stmtUpdate.setInt(3, nuevoCodigo);
 	                    stmtUpdate.setInt(4, id);
 
 	                    int filas = stmtUpdate.executeUpdate();
