9692acb090363e884413718d719cfa4356311d7d
bianca2912 <DAM1@aula11-pc26>
2025-05-15 08:34:39 +0200

    Actualizada CategoriasDAO a objeto

--- Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
+++ Reto3BiancaJorge/src/ClasesDAO/CategoriasDAO.java
@@ -5,35 +5,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import ClasesPK.Categorias;
 import Conexion.Conexion;
 
 public class CategoriasDAO {
 
-    	public static void insertarCategoria(String nombre) {
-            if (nombre.isEmpty()) {
-                System.out.println("El nombre no puede estar vacio.");
-                return;
+	public static void insertarCategoria(Categorias categoria) {
+        String sql = "INSERT INTO categoria (nombre) VALUES (?)";
+
+        try (Connection conn = Conexion.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, categoria.getNombre());
+            int filas = stmt.executeUpdate();
+
+            if (filas > 0) {
+                System.out.println("Categoria insertada correctamente.");
+            } else {
+                System.out.println("No se pudo insertar la categoria.");
             }
 
-            String sql = "INSERT INTO categorias (nombre) VALUES (?)";
-
-            try (Connection conn = Conexion.conectar();
-                 PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-                stmt.setString(1, nombre);
-                int filas = stmt.executeUpdate();
-
-                if (filas > 0) {
-                    System.out.println("Categoria insertada correctamente.");
-                } else {
-                    System.out.println("No se pudo insertar la categoria.");
-                }
-
-            } catch (SQLException e) {
-                System.out.println("Error al insertar categoria.");
-                e.printStackTrace();
-            }
-    } 
-} 
-
+        } catch (SQLException e) {
+            System.out.println("Error al insertar categoria.");
+            e.printStackTrace();
+        }
+    }
+}
 
