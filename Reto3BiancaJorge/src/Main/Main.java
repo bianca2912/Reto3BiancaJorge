0e96d701ddbac42ec0749391c73b9f0be8472ba8
jorge <jorgeruedaplo2004@gmail.com>
2025-05-19 13:52:58 +0200

    Actualizar Main

--- Reto3BiancaJorge/src/Main/Main.java
+++ Reto3BiancaJorge/src/Main/Main.java
@@ -15,14 +15,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		/* do {
+		boolean salir=false;
+		do {
 				menu();
-				numero=FuncionesPK.Funciones.dimeEntero("Elige un menu", sc);
+				int numero=FuncionesPK.Funciones.dimeEntero("Elige un menu", sc);
 				switch (numero) {
 				case 1:
 					do {
 							subMenuMantenimiento();
-							submenu=FuncionesPK.Funciones.dimeEntero("Elige un submenu", sc);
+							int submenu=FuncionesPK.Funciones.dimeEntero("Elige un submenu", sc);
 							switch (submenu) {
 							case 1:
 								System.out.println("Nueva categoria");
@@ -46,56 +47,17 @@ public class Main {
 								ProductosDAO.insertarProducto(p);
 								break;
 							case 3:
-<<<<<<< HEAD
-								menugestionclientes(sc);
-=======
-								do {
-									try {
-										subMenu2Mantenimiento();
-										submenu2=FuncionesPK.Funciones.dimeEntero("Elige otro submenu", sc);
-										switch (submenu2) {
-										case 1:
-											System.out.println("nombre, direccion ");
-											String nombC=sc.nextLine();
-											String dirC=sc.nextLine();
-											int instC=FuncionesPK.Funciones.dimeEntero("codigo", sc);
-											
-											Clientes c=new Clientes(nombC, dirC, instC);
-									
-											ClientesDAO.insertarCliente(c);
-											
-											break;
-										case 2:
-											
-											int codC=FuncionesPK.Funciones.dimeEntero("codigo", sc);
-											
-										//	ClientesDAO.buscarClientePorCodigo(codC);
-											
-											if(ClientesDAO.buscarClientePorCodigo(codC)!=null) {
-												System.out.println(ClientesDAO.buscarClientePorCodigo(codC));
-												
-											}else {
-												System.out.println("No");
-											}
-											
-											break;
-
-										}
-										if (submenu2==0) {
-											System.out.println("Has salido del submenu");
-											break;
-										}
-									} catch (Exception e) {
-										e.printStackTrace();
-									}
-								} while (submenu>0 && submenu<3);
->>>>>>> branch 'main' of https://github.com/bianca2912/Reto3BiancaJorge.git
+								subMenu2Mantenimiento();
+								int num=FuncionesPK.Funciones.dimeEntero("Elige submenu", sc);
+								menugestionclientes(sc,num);
+								
 								break;
 							}
 						
 					} while (numero>0 && numero<4);					
 						break;
 				case 2:
+					int submenu3=0;
 					do {
 							subMenuProductos();
 							submenu3=FuncionesPK.Funciones.dimeEntero("Elige un submenu", sc);
@@ -116,8 +78,8 @@ public class Main {
 					menupedidos(sc);	
 					break;
 				case 4:
-					do {
-						try {
+					/*do {
+				
 							subMenuInformes();
 							submenu5=FuncionesPK.Funciones.dimeEntero("Elige un submenu", sc);
 							switch (submenu5) {
@@ -136,63 +98,24 @@ public class Main {
 								System.out.println("Has salido del submenu");
 								break;
 							}
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+					
 					} while (submenu5>0 && submenu5<4);	
 					break;
+					}*/
+				case 0:
+					salir=true;
+					break;
+				//errorMenu(numero);
 				}
-				errorMenu(numero);
-		
 			
 
-		} while (numero>0 && numero<5);	
-		
-	
-	 	*/
-
-
-		
-		
-		
-	/*	ClientesDao
-	 * 
-	 * 
-		System.out.println("nombre, direccion, codigo");
-		String nombreC=sc.nextLine();
-		String direccionC=sc.nextLine();
-		int codigoC=sc.nextInt();
-		ClientesDAO.insertarCliente(nombreC, direccionC, codigoC);
-	*/
-	/*
-	 * MIRAR
-	 * 
-		System.out.println("idCliente, direccionEnvio, preciototal");
-		int idC=sc.nextInt();
-		String direccionE=sc.nextLine();
-		double precioTotal=sc.nextDouble();
-		PedidosDAO.crearPedido(null, null);
-	*/
-	/*	
-		System.out.println("nombreProducto");
-		String nombreP=sc.nextLine();
-		ProductosDAO.buscarProductoPorNombre(nombreP);
-		*/
-	/*	
-		System.out.println("nombre, talla, color");
-		String nombreP=sc.nextLine();
-		String tallaP=sc.nextLine();
-		String colorP=sc.nextLine();
-		ProductosDAO.buscarProductosConFiltros(nombreP, tallaP, colorP);
-		*/
+		} while (salir);	
 	}
 
-	/*
-	private static void menugestionclientes(Scanner sc) {
-		do {
-			
-				subMenu2Mantenimiento();
-				submenu2=FuncionesPK.Funciones.dimeEntero("Elige otro submenu", sc);
+	
+	private static void menugestionclientes(Scanner sc,int submenu2) {
+				boolean salir=false;
+				do {	
 				switch (submenu2) {
 				case 1:
 					System.out.println("nombre, direccion ");
@@ -208,15 +131,25 @@ public class Main {
 					int codC=FuncionesPK.Funciones.dimeEntero("codigo", sc);
 					
 					ClientesDAO.buscarClientePorCodigo(codC);
+					if(ClientesDAO.buscarClientePorCodigo(codC)!=null) {
+						System.out.println(ClientesDAO.buscarClientePorCodigo(codC));
+						
+					}else {
+						System.out.println("No");
+					}
+					
 					break;
-					
-					
+				case 0:
+					salir=true;
+					break;
 
-				}
+					}
+				} while (salir);
+				
+				
 			
 		
-		} while (submenu>0 && submenu<3);
-	} */
+	}
 	private static void menupedidos(Scanner sc) {
 		int submenu4;
 		do {
